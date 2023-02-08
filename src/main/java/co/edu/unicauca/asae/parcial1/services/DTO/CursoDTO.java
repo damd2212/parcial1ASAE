e1069a5eddc122d992dcd6a0c33f37948dffeb1d
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2023-02-08 11:49:47 -0500

    validaciones en curso

--- src/main/java/co/edu/unicauca/asae/parcial1/services/DTO/CursoDTO.java
+++ src/main/java/co/edu/unicauca/asae/parcial1/services/DTO/CursoDTO.java
@@ -4,6 +4,7 @@ package co.edu.unicauca.asae.parcial1.services.DTO;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -16,8 +17,10 @@ import lombok.Setter;
 public class CursoDTO {
     @NotEmpty(message = "{curso.idCurso.empty}")
     private String idCurso;
+    @NotNull(message = "{curso.nombre.vacio}")
     @Size(min=5,max = 25,message = "{curso.nombre.size}")
     private String nombre;
+    @NotNull(message = "{curso.periodo.vacio}")
     @Min(value=1,message = "{curso.periodo.size}")
     @Max(value=2,message = "{curso.periodo.size}")
     private Integer periodo;
